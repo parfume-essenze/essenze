340341b9621082c13e8e9c0e64f3f05bf4d1bd97
maklersizkvartira <maklersizkvartira@gmail.com>
2026-08-20 20:55:10 +0500

    Fix PORT for Railway deployment

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const { PrismaClient } = require('@prisma/client');
 
 const app = express();
 const prisma = new PrismaClient();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
